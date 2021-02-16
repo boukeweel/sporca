5034011d4dabb5f702156e18d3803c9645322e42
Bouke <BoukeWeel@outlook.com>
2021-02-16 09:48:19 +0100

    space orca behaivor

--- Sporca/Assets/Scripts/Bouke/SpaceOrca.cs
+++ Sporca/Assets/Scripts/Bouke/SpaceOrca.cs
@@ -4,37 +4,39 @@ using UnityEngine;
 
 public class SpaceOrca : MonoBehaviour
 {
-    public Vector3 MovementRoom;
 
+    private Limitations limitations;
+    private Vector3 movementRange;
     
     private float X;
     private float Z;
 
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(transform.position, MovementRoom);
-    }
-
     private void Start()
     {
-        
+        //get out of the limitations script the vector 3 movementroom to use as the movement range
+        limitations = gameObject.GetComponentInParent<Limitations>();
+        movementRange = limitations.MovementRoom;
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        float time = 1f;
+        time -= Time.deltaTime;
+        if (time <= 0)
         {
+            Debug.Log("ja ik kom hier");
             float randomX;
             float randomZ;
 
-            randomX = MovementRoom.x / 2;
-            randomZ = MovementRoom.y / 2;
+            randomX = movementRange.x / 2;
+            randomZ = movementRange.z / 2;
 
             X = Random.Range(-randomX, randomX);
             Z = Random.Range(-randomZ, randomZ);
 
-            transform.Translate(new Vector3(X, 0f, Z) * Time.deltaTime);
+            time = Random.Range(1, 10);
         }
+        float step = 2 * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, new Vector3(X,0f,Z), step);
     }
 }
