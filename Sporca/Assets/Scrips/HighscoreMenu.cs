1d52174e16d6caaed8cbe97d6856c772de4c62ab
Ian Hartog <ianarthur33@gmail.com>
2021-02-17 14:31:18 +0100

    Score board update
    Veranderingen aangebracht aan het score board. Het score board is nog steeds in development.

--- Sporca/Assets/Scrips/HighscoreMenu.cs
+++ Sporca/Assets/Scrips/HighscoreMenu.cs
@@ -5,27 +5,17 @@ using UnityEngine.UI;
 
 public class HighscoreMenu : MonoBehaviour
 {
+    private int score;
+    [SerializeField]
+    private Transform spawnPos;
+    [SerializeField]
+    private Text scoreVisual;
+    [SerializeField]
+    private PointSystem pointSys;
 
-    private Transform entryContainer;
-    private Transform entryTemplate;
-
-    private void Awake()
+    private void Start()
     {
-        entryContainer = transform.Find("highscoreEntryContainer");
-        entryTemplate = entryContainer.Find("highscoreEntryTemplate");
-
-        entryTemplate.gameObject.SetActive(false);
-
-        float templateHeight = 20f;
-        for (int i = 0; i < 10; i++)
-        {
-            Transform entryTransform = Instantiate(entryTemplate, entryContainer);
-            RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
-            entryRectTransform.anchoredPosition = new Vector2(0, -templateHeight * i);
-            entryTransform.gameObject.SetActive(true);
-
-            int rank = i + 1;
-            string rankString;
-        }
+        score = pointSys.Points;
+        scoreVisual.text = score.ToString();
+     }
     }
-}
