b6be89268a9f180b0588b829f87f1bb76b41ddbb
Ian Hartog <ianarthur33@gmail.com>
2021-02-19 11:50:29 +0100

    Placement script added

---
+++ Sporca/Assets/Scrips/PlaneDetection.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.Experimental.XR;
+using System;
+
+public class PlaneDetection : MonoBehaviour
+{
+    private ARSessionOrigin arOrigin;
+    private ARRaycastManager arRaycast;
+    private Pose placementPose;
+    private bool validPlacementPose = false;
+    [SerializeField]
+    private GameObject surounding;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        arOrigin = FindObjectOfType<ARSessionOrigin>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdatePlacementPose();
+        UpdatePlacementIndicator();
+    }
+
+    private void UpdatePlacementIndicator()
+    {
+        if (validPlacementPose)
+        {
+            surounding.SetActive(true);
+            surounding.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+        }
+        else
+        {
+            surounding.SetActive(false);
+        }
+    }
+
+    private void UpdatePlacementPose()
+    {
+        var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        var hits = new List<ARRaycastHit>();
+        arRaycast.Raycast(screenCenter, hits, UnityEngine.XR.ARSubsystems.TrackableType.Planes);
+
+        validPlacementPose = hits.Count > 0;
+        if (validPlacementPose)
+        {
+            placementPose = hits[0].pose;
+        }
+    }
+}
